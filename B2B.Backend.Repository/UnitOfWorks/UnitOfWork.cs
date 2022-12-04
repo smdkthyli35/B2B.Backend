dcf0cd969b4eb7700e9e01233b3b57a6a0ed92b0
Samed Kütahyalı <smdkthyli@gmail.com>
2022-12-04 17:18:26 +0300

    Repository - UnitOfWork added.

---
+++ B2B.Backend.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using B2B.Backend.Core.UnitOfWorks;
+using B2B.Backend.Repository.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly B2BDbContext _context;
+
+        public UnitOfWork(B2BDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
