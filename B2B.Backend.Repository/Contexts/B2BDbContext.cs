8112bfc6213afa3c95a8990523da780414841a9a
Samed Kütahyalı <smdkthyli@gmail.com>
2022-12-04 16:05:21 +0300

    Repository - B2BDbContext added.

---
+++ B2B.Backend.Repository/Contexts/B2BDbContext.cs
@@ -0,0 +1,31 @@
+﻿using B2B.Backend.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Repository.Contexts
+{
+    public class B2BDbContext : DbContext
+    {
+        public B2BDbContext(DbContextOptions<B2BDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerRelationship> CustomerRelationships { get; set; }
+        public DbSet<EmailParameter> EmailParameters { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<PriceList> PriceLists { get; set; }
+        public DbSet<PriceListDetail> PriceListDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
