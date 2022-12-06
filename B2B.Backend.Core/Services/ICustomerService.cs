9a4ba298dc6e90df1abe63c33fe3c9c54e235e16
Samed Kütahyalı <smdkthyli@gmail.com>
2022-12-06 19:08:55 +0300

    Service - IService and Service implementations added.

---
+++ B2B.Backend.Core/Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using B2B.Backend.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Core.Services
+{
+    public interface ICustomerService : IService<Customer>
+    {
+    }
+}
---
+++ B2B.Backend.Service/Services/EmailParameterService.cs
@@ -0,0 +1,19 @@
+﻿using B2B.Backend.Core.Entities;
+using B2B.Backend.Core.Repositories;
+using B2B.Backend.Core.Services;
+using B2B.Backend.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Service.Services
+{
+    public class EmailParameterService : Service<EmailParameter>, IEmailParameterService
+    {
+        public EmailParameterService(IGenericRepository<EmailParameter> repository, IUnitOfWork unitOfWork) : base(repository, unitOfWork)
+        {
+        }
+    }
+}
