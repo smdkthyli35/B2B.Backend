5d25ed147235e5a006d2ba8af2edac8e8d487d7d
Samed Kütahyalı <smdkthyli@gmail.com>
2022-12-06 17:56:56 +0300

    Repository - Added classes implemented from generic repository.

---
+++ B2B.Backend.Repository/Repositories/EmailParameterRepository.cs
@@ -0,0 +1,18 @@
+﻿using B2B.Backend.Core.Entities;
+using B2B.Backend.Core.Repositories;
+using B2B.Backend.Repository.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Repository.Repositories
+{
+    public class EmailParameterRepository : GenericRepository<EmailParameter>, IEmailParameterRepository
+    {
+        public EmailParameterRepository(B2BDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ B2B.Backend.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using B2B.Backend.Core.Entities;
+using B2B.Backend.Core.Repositories;
+using B2B.Backend.Repository.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Repository.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(B2BDbContext context) : base(context)
+        {
+        }
+    }
+}
