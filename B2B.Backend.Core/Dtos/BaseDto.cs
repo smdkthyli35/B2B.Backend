d6be06066433934d6e0dfa5a3c21896af2d385de
Samed Kütahyalı <smdkthyli@gmail.com>
2022-12-07 00:08:24 +0300

    Core - CustomResponseDto, BaseDto and NoContentDto added.

---
+++ B2B.Backend.Core/Dtos/BaseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2B.Backend.Core.Dtos
+{
+    public abstract class BaseDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
